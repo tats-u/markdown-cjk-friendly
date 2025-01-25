06278c41f74318c4781815e3fa6d552dd9c23bb8
Tatsunori Uchino <tats.u@live.jp>
2025-01-25 20:49:02 +0900

    chore: improve test cases

--- packages/markdown-it-cjk-friendly/test/markdownIt.spec.ts
+++ packages/markdown-it-cjk-friendly/test/markdownIt.spec.ts
@@ -5,12 +5,22 @@ import commonMarkTestCases from "../../../testcases/commonmark.json" with {
   type: "json",
 };
 
-const md = MarkdownIt();
+const md = new MarkdownIt();
 md.use(markdownItCjkFriendly);
+
+const mdOriginal = new MarkdownIt();
+
+function md2Html(input: string): string {
+  return md.render(input);
+}
+
+function md2HtmlOriginal(input: string): string {
+  return mdOriginal.render(input);
+}
 
 describe("markdown-it-cjk-friendly", () => {
   it("** around Kana/Han is converted to <strong>", async () => {
-    const result = md.render(
+    const result = md2Html(
       await readFile(
         new URL("../../../testcases/strong.md", import.meta.url),
         "utf-8",
@@ -23,7 +33,7 @@ describe("markdown-it-cjk-friendly", () => {
   });
 
   it("** around Korean is converted to <strong>", async () => {
-    const result = md.render(
+    const result = md2Html(
       await readFile(
         new URL("../../../testcases/korean.md", import.meta.url),
         "utf-8",
@@ -36,7 +46,7 @@ describe("markdown-it-cjk-friendly", () => {
   });
 
   it("** around pseudo-emoji (CJK symbols that are also unqualified-emoji) is converted to <strong>", async () => {
-    const result = md.render(
+    const result = md2Html(
       await readFile(
         new URL("../../../testcases/pseudo-emoji.md", import.meta.url),
         "utf-8",
@@ -49,7 +59,7 @@ describe("markdown-it-cjk-friendly", () => {
   });
 
   it("recognizes non-BMP punctuation and symbols", async () => {
-    const result = md.render(
+    const result = md2Html(
       await readFile(
         new URL("../../../testcases/non-bmp.md", import.meta.url),
         "utf-8",
@@ -62,7 +72,7 @@ describe("markdown-it-cjk-friendly", () => {
   });
 
   it("process underscores around CJK punctuation", async () => {
-    const result = md.render(
+    const result = md2Html(
       await readFile(
         new URL("../../../testcases/underscore-cjk-punct.md", import.meta.url),
         "utf-8",
@@ -83,20 +93,18 @@ describe("markdown-it-cjk-friendly", () => {
     if (!markdownRegexResult) {
       throw new Error("Failed to find example Markdown in README");
     }
-    const result = md.render(markdownRegexResult[1]);
+    const result = md2Html(markdownRegexResult[1]);
     for (const line of result.split(/\r?\n/)) {
       expect(line).not.toMatch(/\*\*[^\n]+\*\*/);
     }
     expect(result).toMatchSnapshot();
   });
 
-  it("Don't conflict with existing CommonMark test cases", async () => {
+  it("Output for CommonMark test cases are the same as those without this plugin", async () => {
     for (const testCase of commonMarkTestCases) {
-      if (testCase.section !== "Emphasis and strong emphasis") continue;
-      // markdown-it is not incompatible with test cases containing raw HTML tags
-      if (/<[a-z]+ [^>]+=[^>]+>/.test(testCase.markdown)) continue;
-      const result = md.render(testCase.markdown);
-      expect(result).toBe(testCase.html);
+      expect(md2Html(testCase.markdown)).toBe(
+        md2HtmlOriginal(testCase.markdown),
+      );
     }
   });
 });
