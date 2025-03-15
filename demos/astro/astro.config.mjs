a9ac0c9908fbd305675010b7b922d3b52c967ebd
Tatsunori Uchino <tats.u@live.jp>
2025-03-15 17:53:06 +0900

    Add Astro demo

---
+++ demos/astro/astro.config.mjs
@@ -0,0 +1,24 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import remarkCjkFriendly from "remark-cjk-friendly";
+import remarkCjkFriendlyGfmStrikethrough from "remark-cjk-friendly-gfm-strikethrough";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    
+    mdx(
+      // You can set it below in markdown, so you don't need to set it here again.
+      // {
+      //   gfm: true,
+      //   remarkPlugins: [remarkCjkFriendly, remarkCjkFriendlyGfmStrikethrough],
+      // }
+    ),
+  ],
+  markdown: {
+    gfm: true,
+    // If you turn off `gfm`, `remarkCjkFriendlyGfmStrikethrough` is not needed.
+    remarkPlugins: [remarkCjkFriendly, remarkCjkFriendlyGfmStrikethrough],
+  }
+});
---
+++ demos/astro/src/pages/index.md
@@ -0,0 +1,12 @@
+---
+layout: ../layouts/Layout.astro
+title: Astro Demo
+---
+
+# Astro Demo
+
+普通はこのような強調記号は認識されませんが**[`remark-cjk-friendly`プラグイン](https://npmjs.com/package/remark-cjk-friendly)のおかげで認識されます。**やったね！
+
+~~[`remark-cjk-friendly-gfm-strikethrough`プラグイン](https://npmjs.com/package/remark-cjk-friendly-gfm-strikethrough)はダメかな？~~ちゃんと動いてくれました。
+
+[MDX Demo](mdx)
