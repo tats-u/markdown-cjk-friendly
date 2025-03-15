7cf58b9e3e652a7820a794e100addc3ed28a2519
Tatsunori Uchino <tats.u@live.jp>
2025-03-15 18:03:19 +0900

    Format

--- demos/astro/astro.config.mjs
+++ demos/astro/astro.config.mjs
@@ -1,13 +1,12 @@
+import mdx from "@astrojs/mdx";
 // @ts-check
 import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
 import remarkCjkFriendly from "remark-cjk-friendly";
 import remarkCjkFriendlyGfmStrikethrough from "remark-cjk-friendly-gfm-strikethrough";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
-    
     mdx(
       // You can set it below in markdown, so you don't need to set it here again.
       // {
@@ -20,5 +19,5 @@ export default defineConfig({
     gfm: true,
     // If you turn off `gfm`, `remarkCjkFriendlyGfmStrikethrough` is not needed.
     remarkPlugins: [remarkCjkFriendly, remarkCjkFriendlyGfmStrikethrough],
-  }
+  },
 });
