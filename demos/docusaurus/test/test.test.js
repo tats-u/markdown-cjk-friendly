c95512c52e5d76c24a3a93fdcf2d63c76374edb9
Tatsunori Uchino <tats.u@live.jp>
2025-03-19 22:02:04 +0900

    Add test on pages generated by Docusaurus

---
+++ demos/docusaurus/test/test.test.js
@@ -0,0 +1,88 @@
+import { readFile } from "node:fs/promises";
+import { JSDOM } from "jsdom";
+import { beforeAll, describe, expect, it } from "vitest";
+
+async function loadHtml(path) {
+  return await readFile(new URL(path, import.meta.url), "utf-8");
+}
+
+describe("index.html (pages)", () => {
+  /** @type {HTMLElement} */
+  let markdown = undefined;
+  beforeAll(async () => {
+    const source = await loadHtml("../build/index.html");
+    markdown = new JSDOM(source).window.document.querySelector("article");
+    expect(markdown).not.toBeNull();
+  });
+
+  it("contains <strong>", () => {
+    expect(markdown.querySelector("strong")).not.toBeNull();
+  });
+  it("contains <del>", () => {
+    expect(markdown.querySelector("del")).not.toBeNull();
+  });
+  it("paragraphs don't contain **", () => {
+    for (const paragraph of markdown.querySelectorAll("p")) {
+      expect(paragraph.innerHTML).not.toMatch(/\*\*[^\n]+\*\*/);
+    }
+  });
+  it("paragraphs don't contain ~~", () => {
+    for (const paragraph of markdown.querySelectorAll("p")) {
+      expect(paragraph.innerHTML).not.toMatch(/~~[^\n]+~~/);
+    }
+  });
+});
+
+describe("docs/index.html", () => {
+  /** @type {HTMLElement} */
+  let markdown = undefined;
+  beforeAll(async () => {
+    const source = await loadHtml("../build/docs/index.html");
+    markdown = new JSDOM(source).window.document.querySelector("article");
+    expect(markdown).not.toBeNull();
+  });
+
+  it("contains <strong>", () => {
+    expect(markdown.querySelector("strong")).not.toBeNull();
+  });
+  it("contains <del>", () => {
+    expect(markdown.querySelector("del")).not.toBeNull();
+  });
+  it("paragraphs don't contain **", () => {
+    for (const paragraph of markdown.querySelectorAll("p")) {
+      expect(paragraph.innerHTML).not.toMatch(/\*\*[^\n]+\*\*/);
+    }
+  });
+  it("paragraphs don't contain ~~", () => {
+    for (const paragraph of markdown.querySelectorAll("p")) {
+      expect(paragraph.innerHTML).not.toMatch(/~~[^\n]+~~/);
+    }
+  });
+});
+
+describe("blog/index.html", () => {
+  /** @type {HTMLElement} */
+  let markdown = undefined;
+  beforeAll(async () => {
+    const source = await loadHtml("../build/blog/index.html");
+    markdown = new JSDOM(source).window.document.querySelector("article");
+    expect(markdown).not.toBeNull();
+  });
+
+  it("contains <strong>", () => {
+    expect(markdown.querySelector("strong")).not.toBeNull();
+  });
+  it("contains <del>", () => {
+    expect(markdown.querySelector("del")).not.toBeNull();
+  });
+  it("paragraphs don't contain **", () => {
+    for (const paragraph of markdown.querySelectorAll("p")) {
+      expect(paragraph.innerHTML).not.toMatch(/\*\*[^\n]+\*\*/);
+    }
+  });
+  it("paragraphs don't contain ~~", () => {
+    for (const paragraph of markdown.querySelectorAll("p")) {
+      expect(paragraph.innerHTML).not.toMatch(/~~[^\n]+~~/);
+    }
+  });
+});
