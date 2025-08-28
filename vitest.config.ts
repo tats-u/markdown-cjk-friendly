47ceab567e524a005d300fc52414ed02a05938f6
Tatsunori Uchino <tats.u@live.jp>
2025-08-28 23:47:41 +0900

    Enable CodSpeed bench

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,10 @@
+import codspeedPlugin from "@codspeed/vitest-plugin";
 import AutoImport from "unplugin-auto-import/vite";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   plugins: [
+    codspeedPlugin(),
     AutoImport({
       imports: ["vitest"],
       dts: true, // generate TypeScript declaration
