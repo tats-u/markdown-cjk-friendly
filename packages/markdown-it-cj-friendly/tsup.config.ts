a90a721c6b37f144db0bb7da2f3a09502f95a691
Tatsunori Uchino <tats.u@live.jp>
2025-09-06 19:08:45 +0900

    Change target platform from node to neutral in tsup config

--- packages/markdown-it-cj-friendly/tsup.config.ts
+++ packages/markdown-it-cj-friendly/tsup.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "tsup";
 export default defineConfig({
   entry: ["./src"],
   format: ["esm", "cjs"],
+  platform: "neutral",
   splitting: false,
   dts: true,
   target: "es2024",
